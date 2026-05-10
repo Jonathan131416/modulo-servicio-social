af2655b9f38b38c4696a6e24f08fd16592e0f150
Jonathan131416 <gengar12345689@gmail.com>
2026-05-09 21:43:06 -0600

    usar ngrok para HTTPS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({ 
-  baseURL: 'http://98.85.123.202:8000/api',
+  baseURL: 'https://rectify-tapeless-setup.ngrok-free.dev/api',
   maxRedirects: 0
 });
 api.interceptors.request.use((config) => {
